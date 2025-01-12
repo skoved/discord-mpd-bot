e19ce72ca648f7f8814884f7473d5f61c85ec69d
skoved <sam.koved@gmail.com>
2025-01-11 22:21:16 -0500

    connect to mpd, then get uri for song using yt-dlp, then add song to mpd queue, and finally unpause mpd if mpd is not already playing
    Signed-off-by: skoved <sam.koved@gmail.com>

---
+++ main.go
@@ -0,0 +1,67 @@
+// discord-mpd-bot
+// Copyright (C) 2025 skoved
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/fhs/gompd/v2/mpd"
+	"github.com/lrstanley/go-ytdlp"
+)
+
+func main() {
+	client, err := mpd.Dial("tcp", "localhost:6600")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Could not connect to MPD. Check to make sure that MPD is running:", err.Error())
+		os.Exit(1)
+	}
+	statusAttrs, err := client.Status()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Could not get the state of MPD:", err.Error())
+		os.Exit(1)
+	}
+	fmt.Println("State:", statusAttrs["state"])
+	if len(os.Args) >= 1 {
+		dl := ytdlp.New()
+		dl.GetURL()
+		ctx, cancel := context.WithTimeout(context.Background(), 120*time.Second)
+		defer cancel()
+		res, err := dl.Run(ctx, os.Args[1])
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Could not get song:", os.Args[1])
+			os.Exit(1)
+		}
+		song := strings.Split(res.Stdout, "\n")
+		fmt.Println("next song:", song[len(song)-1])
+		err = client.Add(song[len(song)-1])
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Could not queue song", song[len(song)-1]+":", err.Error())
+			os.Exit(1)
+		}
+	}
+	if statusAttrs["state"] != "play" {
+		err = client.Pause(false)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Could not unpause MPD:", err.Error())
+			os.Exit(1)
+		}
+	}
+}
